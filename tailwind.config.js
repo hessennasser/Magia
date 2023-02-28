0919e1d7635f29bfee5a60fbc40a9ffa27c0b6c8
Hessen Nasser <hessenaloosh@gmail.com>
2023-02-28 18:22:34 +0200

    ui/ux section in home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-blue': '#083e96',
+        'dark-bg': '#263238',
+      },
+    },
+    fontFamily: {
+      'roboto': "'Roboto', sans-serif",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        'lg': '5rem',
+        'md': '2.5rem',
+        'sm': '1rem',
+      },
+    },
   },
   plugins: [],
-}
+};
