2a1988c16282ac5269c2f10a96d12568571154bd
Hessen Nasser <hessenaloosh@gmail.com>
2023-03-02 00:56:28 +0200

    home page and sign in page in addition to header

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector("nav");
+
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active")
+    nav.classList.toggle("active")
+})
