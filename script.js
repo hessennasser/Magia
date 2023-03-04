ed7b4b15ed26e213c660e6853743e28877013b03
Hessen Nasser <hessenaloosh@gmail.com>
2023-03-04 19:10:16 +0200

    add projects section and title for main sections

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("active");
   nav.classList.toggle("active");
 });
-// image member
-const imageMember = document.querySelector(".image-member");
-imageMember.addEventListener("click", () => {
-  window.open("https://google.com");
-});
+
 
 const footer = document.querySelector("footer p");
 
