744b2f6b879d04259d93a49064230cede10c2857
Philopatire <philofolfol@gmail.com>
2023-06-21 11:17:19 +0300

    Style color button

--- script.js
+++ script.js
@@ -6,43 +6,42 @@ menuIcon.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
 
-
 const footer = document.querySelector("footer p");
 
 footer.innerHTML += new Date().getFullYear();
 
+let get_started_btn_UI = document.querySelector(".main-btn");
+get_started_btn_UI.onclick = () => {
+  window.open("https://www.facebook.com/profile.php?id=100090867099566");
+};
 
-let get_started_btn_UI =  document.querySelector(".main-btn");
-get_started_btn_UI.onclick = () =>{
-  window.open("https://www.facebook.com/profile.php?id=100090867099566")
-}
+let facebook_icon = document.querySelector(".facebook");
+facebook_icon.onclick = () => {
+  window.open("https://www.facebook.com/profile.php?id=100090867099566");
+};
 
-let facebook_icon = document.querySelector(".facebook")
-facebook_icon.onclick = () =>{
-  window.open("https://www.facebook.com/profile.php?id=100090867099566")
-}
+let whatsApp_icon = document.querySelector(".whatsApp");
 
-let whatsApp_icon = document.querySelector(".whatsApp")
+whatsApp_icon.onclick = () => {
+  alert("Thanks for your Visit 😃😃");
+  window.open("https://wa.me/message/6IZY2XJO2XFQN1?src=qr");
+};
 
-whatsApp_icon.onclick = ()=>{
-  alert("Thanks for your Visit 😃😃")
-  window.open("https://wa.me/message/6IZY2XJO2XFQN1?src=qr")
-}
+linkedin_icon = document.querySelector(".linkedin");
 
-linkedin_icon = document.querySelector(".linkedin")
-
-linkedin_icon.onclick = () =>{
-  alert("Thanks for your Visit 😃😃 ")
-  window.open("https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/")
-} 
+linkedin_icon.onclick = () => {
+  alert("Thanks for your Visit 😃😃 ");
+  window.open("https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/");
+};
 
 // change color
 
+let change = document.querySelector("label .change_color_input");
+let labelChange = document.querySelector("label.change_color");
 
-let change = document.querySelector(".change_color");
-
-document.addEventListener("input",function(){
+document.addEventListener("input", function () {
   let changeColor = change.value;
-  
-  document.body.style.backgroundColor = changeColor
+
+  labelChange.style.backgroundColor = changeColor;
+  document.body.style.backgroundColor = changeColor;
 });
